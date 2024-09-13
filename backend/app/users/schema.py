33fd46fbfec98be2d0ef291da4f1b7f46daad559
Rudolf Bakhman <rudik.login@gmail.com>
2024-09-13 16:41:19 +0300

    change response to user data

--- backend/app/users/schema.py
+++ backend/app/users/schema.py
@@ -1,4 +1,5 @@
 from pydantic import BaseModel, Field
+from typing import Union
 import datetime
 
 
@@ -6,10 +7,10 @@ class User(BaseModel):
     id: int = Field(examples=[1])
     first_name: str = Field(examples=["Имя"])
     last_name: str = Field(examples=["Фамилия"])
-    birthday: datetime.date  = Field(examples=["1998-01-01"])
-    gender: str = Field(examples=["Пол"])
-    hobby: str = Field(examples=["Хобби, интересы и т.п."])
-    city: str = Field(examples=["Город"])
+    birthday: Union[datetime.date, None] = Field(examples=["1998-01-01"])
+    gender: Union[str, None] = Field(examples=["Пол"])
+    hobby: Union[str, None] = Field(examples=["Хобби, интересы и т.п."])
+    city: Union[str, None] = Field(examples=["Город"])
 
 
 class CreateUser(BaseModel):
