09005de7948ee75a3887c49e6bea278382805423
Rudolf Bakhman <rudik.login@gmail.com>
2024-09-27 19:15:15 +0300

    Add users to sql

--- backend/migrate/insert_users.sh
+++ backend/migrate/insert_users.sh
@@ -2,12 +2,12 @@
 while read -r line;
 do
   IFS=","
-  set -- "$line"
+  set -- $line
   declare -a PARAMS="( $1 )"
   first_name="${PARAMS[0]}"
   last_name="${PARAMS[1]}"
   data=$2
   city=$3
 
-  psql -U admin -d api -c "INSERT INTO users (first_name, last_name, birthday, city, password, login) VALUES($first_name, $last_name, $data, $city, $first_name, $last_name)"
+  psql -U admin -d api -c "INSERT INTO users (first_name, last_name, birthday, city, password, login) VALUES('$first_name', '$last_name', '$data', '$city', '$first_name', '$last_name')"
 done < users.txt
