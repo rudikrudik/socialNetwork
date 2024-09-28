f4e486ad38c3526f2bb0f9c1a4f572f231ad5244
Rudolf Bakhman <rudik.login@gmail.com>
2024-09-28 11:05:58 +0300

    Add search users and sort by id

--- backend/app/db/user.py
+++ backend/app/db/user.py
@@ -18,7 +18,8 @@ def find_user_by_login(login: str) -> dict:
 
 def search_users(first_name: str, last_name: str) -> dict:
     return db.query(f"SELECT id, first_name, last_name, birthday, gender, hobby, city FROM users "
-                    f"WHERE first_name LIKE '{first_name}%' AND last_name LIKE '{last_name}%';")
+                    f"WHERE first_name LIKE '{first_name}%' AND last_name LIKE '{last_name}%'"
+                    f"ORDER BY id;")
 
 
 def auth_user(login: str) -> dict:
