4f9f7ed94880680677488046ebcebbc3af82bc05
Rudolf Bakhman <rudik.login@gmail.com>
2025-05-29 16:34:43 +0300

    add separate connect to db

--- backend/app/db/sql.py
+++ backend/app/db/sql.py
@@ -1,28 +1,32 @@
 import psycopg, time
-from app.config import settings
 
 
 class Database:
     def __init__(self, name, user, password, host, port):
-        for i in range(1, 6):
-            try:
-                self.connect = psycopg.connect(dbname=name,
-                                                user=user,
-                                                password=password,
-                                                host=host,
-                                                port=port)
-                self.cursor = self.connect.cursor()
-                check_error = None
-            except psycopg.Error as error:
-                print(f"Retry to connect to database. Attempt: {i}")
-                check_error = error
-            if i == 5:
-                raise check_error
-            if check_error:
-                time.sleep(5)
-            else:
-                print(f"Success connect to database. Attempt: {i}")
-                break
+        self.db_name = name
+        self.db_user = user
+        self.db_password = password
+        self.db_host = host
+        self.db_port = port
+        self.connect = psycopg.connect(dbname=self.db_name,
+                                           user=self.db_user,
+                                           password=self.db_password,
+                                           host=self.db_host,
+                                           port=self.db_port)
+        self.cursor = self.connect.cursor()
+
+    def connect(self):
+        print("FROM PRINT")
+        try:
+            self.connect = psycopg.connect(dbname=self.db_name,
+                                           user=self.db_user,
+                                           password=self.db_password,
+                                           host=self.db_host,
+                                           port=self.db_port)
+            self.cursor = self.connect.cursor()
+            print("Connect successful")
+        except psycopg.Error as error:
+            print(f"Retry to connect to database")
 
     def close(self):
         self.cursor.close()
@@ -43,9 +47,3 @@ class Database:
     def insert(self, insert_query):
         self.cursor.execute(insert_query)
         self.connect.commit()
-
-db = Database(settings.DB_NAME,
-              settings.DB_USER,
-              settings.DB_PASSWORD,
-              settings.DB_HOST,
-              settings.DB_PORT)
--- backend/app/db/user.py
+++ backend/app/db/user.py
@@ -1,17 +1,29 @@
-from app.db.sql import db
+from app.db.sql import Database as data_base
 from app.users.auth import get_hashed_password
+from app.config import settings
+
+
+def db_query_read(query: str) -> dict:
+    db = data_base(settings.DB_NAME,
+                  settings.DB_USER,
+                  settings.DB_PASSWORD,
+                  settings.DB_HOST,
+                  settings.DB_PORT)
+    result = db.query_one(query)
+    db.close()
+    return result
 
 
 def get_all_users() -> dict:
-    return db.query("SELECT first_name, last_name, birthday, gender, hobby, city FROM users;")
+    return db_query_read("SELECT first_name, last_name, birthday, gender, hobby, city FROM users;")
 
 
 def get_user_by_id(id: int) -> dict:
-    return db.query_one(f"SELECT * FROM users WHERE id = {id};")
+    return db_query_read(f"SELECT * FROM users WHERE id = {id};")
 
 
 def find_user_by_login(login: str) -> dict:
-    result = db.query(f"SELECT id FROM users WHERE login='{login}';")
+    result = db_query_read(f"SELECT id FROM users WHERE login='{login}';")
     return result[0] if result else None
 
 # Sql select with no indexes
@@ -21,13 +33,13 @@ def find_user_by_login(login: str) -> dict:
 
 
 def search_users(first_name: str, last_name: str) -> dict:
-    return db.query(f"SELECT id, first_name, last_name, birthday, gender, hobby, city F"
+    return db_query_read(f"SELECT id, first_name, last_name, birthday, gender, hobby, city F"
                     f"ROM users WHERE LOWER(first_name)::text LIKE '{first_name.lower()}%'"
                     f" AND LOWER(last_name)::text LIKE '{last_name.lower()}%';")
 
 
 def auth_user(login: str) -> dict:
-    result = db.query(f"SELECT id, login, password FROM users WHERE login = '{login}';")
+    result = db_query_read(f"SELECT id, login, password FROM users WHERE login = '{login}';")
     return result[0] if result else None
 
 
