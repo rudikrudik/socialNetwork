e24c37122c7b1a3d76cd29adae345c2e5bf85070
Rudolf Bakhman <rudik.login@gmail.com>
2025-05-29 16:38:27 +0300

    add separate connect to db

--- backend/app/db/user.py
+++ backend/app/db/user.py
@@ -10,7 +10,6 @@ def db_query_read(query: str) -> dict:
                   settings.DB_HOST,
                   settings.DB_PORT)
     result = db.query_one(query)
-    print("data")
     db.close()
     return result
 
