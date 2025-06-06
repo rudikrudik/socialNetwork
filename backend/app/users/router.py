fa1312c7877c868edaa2350f23f3b28e5fc2af2e
Rudolf Bakhman <rudik.login@gmail.com>
2025-06-07 00:58:18 +0300

    add get user friends\posts

--- backend/app/users/router.py
+++ backend/app/users/router.py
@@ -126,7 +126,7 @@ def get_user_posts(token: str = Depends(dep.get_token)):
 def get_user_posts(id_post: IdUser, token: str = Depends(dep.get_token)):
     user_id = dep.get_current_user(token)
     try:
-        result = db_user.delete_user_post(user_id, id_post)
+        db_user.delete_user_post(user_id, id_post.id)
         return {"Post Delete:", f"{id_post}"}
     except BaseException:
         raise HTTPException(
