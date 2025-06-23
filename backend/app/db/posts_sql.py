fc4a613fa493fa8cecca89e03d95535340f7a463
Rudolf Bakhman <rudik.login@gmail.com>
2025-06-23 16:13:25 +0300

    add redis cache post feed

--- backend/app/db/posts_sql.py
+++ backend/app/db/posts_sql.py
@@ -24,22 +24,14 @@ def update_user_post(id_user: int, post_content: str):
 
 
 def get_user_post_by_id(id_post: int) -> tuple:
-    print("Get user post by id: ", id_post)
     try:
         result_from_redis = redis_db_proxy_get_query_key(id_post)
-        print("Result from redis: ", result_from_redis)
-
-        if result_from_redis is not None:
-            return result_from_redis
-        else:
-            result_from_sql = raw_query(f"SELECT * FROM user_posts WHERE id = {id_post}", True)
-            print("Result from sql: ", result_from_sql)
-            redis_db_proxy_set_query_key(id_post, result_from_sql)
-
-        return result_from_sql
-
+        print(result_from_redis)
     except BaseException as e:
-        print("Error ", e)
+        print(f"Error {e}")
+
+    result_from_sql = raw_query(f"SELECT * FROM user_posts WHERE id = {id_post}", True)
+    return result_from_sql
 
 
 def get_post_limit_and_offset(user_id: int, posts_limit: int, offset: int):
