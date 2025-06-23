447e92fd44aa187127283e51c5cabffff4d8c619
Rudolf Bakhman <rudik.login@gmail.com>
2025-06-23 15:39:09 +0300

    add redis cache post feed

--- backend/app/redis_cache/redis_db.py
+++ backend/app/redis_cache/redis_db.py
@@ -27,4 +27,5 @@ def redis_db_proxy_set_query_key(id_post, result_from_sql: tuple) -> None:
     r = redis_connect()
     dict_str = json.dumps(result_from_sql, indent=4, sort_keys=True, default=str)
     r.hset(str(id_post), dict_str)
+    print(r.ping())
     print("From redis", r.get(str(id_post)))
