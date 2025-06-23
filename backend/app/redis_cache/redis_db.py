dd526efc1350367151037d5b4e96c9e9274187bc
Rudolf Bakhman <rudik.login@gmail.com>
2025-06-23 15:44:10 +0300

    add redis cache post feed

--- backend/app/redis_cache/redis_db.py
+++ backend/app/redis_cache/redis_db.py
@@ -20,6 +20,7 @@ def redis_connect() -> redis:
 def redis_db_proxy_get_query_key(id_post: int) -> tuple:
     r = redis_connect()
     print("Redis ping: ", r.ping())
+    print("Redis post 42: ", r.get(str(id_post)))
     return r.get(str(id_post)) if r.exists(str(id_post)) else None
 
 
