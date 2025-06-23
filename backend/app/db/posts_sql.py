bac3c2206d97244de93fbffd22c1837d9ac6c50b
Rudolf Bakhman <rudik.login@gmail.com>
2025-06-23 17:08:33 +0300

    add redis cache post feed

--- backend/app/db/posts_sql.py
+++ backend/app/db/posts_sql.py
@@ -29,7 +29,7 @@ def get_user_post_by_id(id_post: int) -> tuple:
         return result_from_redis
     else:
         result_from_sql = raw_query(f"SELECT * FROM user_posts WHERE id = {id_post}", True)
-        redis_db_proxy_set_query_key(id_post, result_from_redis)
+        redis_db_proxy_set_query_key(id_post, result_from_sql)
         return result_from_sql
 
 
