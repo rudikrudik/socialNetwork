cb1e64fddc3a545f73635c7596a736d4aa58b553
Rudolf Bakhman <rudik.login@gmail.com>
2025-06-26 22:28:56 +0300

    add infinity scroll to news feed

--- backend/app/db/posts_sql.py
+++ backend/app/db/posts_sql.py
@@ -15,15 +15,16 @@ def delete_user_post(id_user: int, id_post: int):
 
 
 def create_user_post(id_post: int, post_content: str):
-    redis_db_proxy_delete_key(id_post)
-
     return raw_query(f"INSERT INTO user_posts (user_id, post_date_create, post_content) VALUES ({id_post},"
                      f"date_trunc('second', now()::timestamp), '{post_content}');",
                      True, settings.DB_PORT_WRITE)
 
 
-def update_user_post(id_user: int, post_content: str):
-    return raw_query(f"UPDATE user_posts SET post_content = '{post_content}' WHERE id = {id_user}",
+def update_user_post(id_post: int, post_content: str):
+    # Delete not edited post from cache
+    redis_db_proxy_delete_key(id_post)
+
+    return raw_query(f"UPDATE user_posts SET post_content = '{post_content}' WHERE id = {id_post}",
                      True, settings.DB_PORT_WRITE)
 
 
@@ -40,6 +41,7 @@ def get_user_post_by_id(id_post: int) -> tuple:
 
 
 def get_post_limit_and_offset(user_id: int, posts_limit: int, offset: int):
-    return raw_query(f"SELECT * FROM user_posts WHERE user_id IN "
-                     f"(SELECT friend_id FROM user_friends WHERE user_id = {user_id}) "
+    return raw_query(f"SELECT * FROM user_posts WHERE user_id IN"
+                     f"(SELECT friend_id FROM user_friends WHERE user_id = {user_id})"
+                     f"ORDER BY post_date_create DESC"
                      f"LIMIT {posts_limit} OFFSET {offset}", False)
