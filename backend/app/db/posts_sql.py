3299efc93f6dadcc065d551bd6d277f4619415cd
Rudolf Bakhman <rudik.login@gmail.com>
2025-06-27 11:48:03 +0300

    add infinity scroll to news feed

--- backend/app/db/posts_sql.py
+++ backend/app/db/posts_sql.py
@@ -46,10 +46,9 @@ def get_post_limit_and_offset(user_id: int, posts_limit: int, offset: int) -> li
     id_posts = raw_query(f"SELECT (id) FROM user_posts WHERE user_id IN"
                          f"(SELECT friend_id FROM user_friends WHERE user_id = {user_id}) "
                          f"ORDER BY post_date_create DESC LIMIT {posts_limit} OFFSET {offset}", False)
-    print("Id post:", id_posts)
 
     if id_posts:
         for id_post in id_posts:
-            list_posts.extend(get_user_post_by_id(id_post))
+            list_posts.extend(get_user_post_by_id(*id_post))
 
     return list_posts
