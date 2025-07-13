078a92ca00855f818979aa2eb405e9bbb8cc0b10
Rudolf Bakhman <rudik.login@gmail.com>
2025-07-13 17:39:48 +0300

    add mongo DB to dialogs

--- backend/app/mongo_db/mongo_db.py
+++ backend/app/mongo_db/mongo_db.py
@@ -4,8 +4,6 @@ from pymongo.errors import ConnectionFailure, ServerSelectionTimeoutError, Opera
 from datetime import datetime
 from pymongo.synchronous.collection import Collection
 from app.config import settings
-from bson import json_util
-import json
 
 
 def mongodb_connect() -> Collection[Mapping[str, Any] | Any]:
@@ -33,6 +31,6 @@ def mongodb_insert_one(id_from: int, to_id: int, content: str) -> str:
 
 def mongodb_query(id_from: int) -> list:
     try:
-        return json.loads(json_util.dumps(mongodb_connect().find({"id_from": id_from})))
+        return list(mongodb_connect().find({"id_from": id_from}, {'_id': False}))
     except OperationFailure as error:
         print(f"Query error {error}")
