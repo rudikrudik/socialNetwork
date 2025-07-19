0af11f6e3b94e529b9c42f97887b561574568062
Rudolf Bakhman <rudik.login@gmail.com>
2025-07-19 17:51:08 +0300

    posts feed websockets

--- post_notification/app/manager.py
+++ post_notification/app/manager.py
@@ -1,21 +1,26 @@
 from typing import Any
-
+import json
 from starlette.websockets import WebSocket
 
 
 class ConnectionManager:
     def __init__(self):
-        self.active_user_connections: list[WebSocket] = []
+        self.active_user_connections: dict = {}
         self.user_friends: dict = {}
 
     async def connect(self, websocket: WebSocket):
         await websocket.accept()
-        self.active_user_connections.append(websocket)
+        data = await websocket.receive_text()
+        id_user = json.loads(data)
+
+        self.active_user_connections[id_user["id_user"]] = websocket
         for i in self.active_user_connections:
             print(f"Active Connections: {i}", flush=True)
 
-    def disconnect(self, websocket: WebSocket):
-        self.active_user_connections.remove(websocket)
+    async def disconnect(self, websocket: WebSocket):
+        data = await websocket.receive_text()
+        id_user = json.loads(data)
+        del self.active_user_connections[id_user["id_user"]]
 
     async def send_message(self, message: str):
         print(f"get message: {message}", flush=True)
