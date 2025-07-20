7182a8d60017bf9498709a8d0c8818aa6e45d8fe
Rudolf Bakhman <rudik.login@gmail.com>
2025-07-20 14:21:06 +0300

    posts feed websockets

--- post_notification/app/manager.py
+++ post_notification/app/manager.py
@@ -22,10 +22,10 @@ class ConnectionManager:
         del self.active_user_connections[id_user["id_user"]]
 
     async def send_message(self, message):
-        ws_recieve = json.loads(message)
         print(f"get message: {message}", flush=True)
         #print("Friends list", ws_recieve["friends"], "Type: ", type(ws_recieve["friends"]))
-
+        json_string = json.dumps(message)
+        print(json_string)
         # for friend in message["friends"]:
         #     if friend in self.active_user_connections:
         #         await self.active_user_connections[friend].send(f"New post from {message["id"]}")
