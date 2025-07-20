8dbb973ad9b722868891ff828e52ec2023a98a6c
Rudolf Bakhman <rudik.login@gmail.com>
2025-07-20 14:58:31 +0300

    posts feed websockets

--- post_notification/app/manager.py
+++ post_notification/app/manager.py
@@ -12,21 +12,16 @@ class ConnectionManager:
         id_user = json.loads(data)
 
         self.active_user_connections[int(id_user["id_user"])] = websocket
-        for i in self.active_user_connections:
-            print(f"Active Connections: {i}", flush=True)
 
     async def disconnect(self, websocket: WebSocket):
         data = await websocket.receive_text()
         id_user = json.loads(data)
-        del self.active_user_connections[id_user["id_user"]]
+        del self.active_user_connections[int(id_user["id_user"])]
 
     async def send_message(self, user_id, friends):
         for friend in friends:
-            print(f"Type friend: {friend}")
             if friend in self.active_user_connections.keys():
                 await self.active_user_connections[friend].send_text(f"New post from {user_id}")
-            else:
-                print(f"{friend} not in active connection")
 
 
 manager = ConnectionManager()
