585207610f8fa45b9a5c19802cbe755064d5863d
Rudolf Bakhman <rudik.login@gmail.com>
2025-07-22 16:58:08 +0300

    add rabbitMQ queue

--- post_notification/app/manager.py
+++ post_notification/app/manager.py
@@ -10,13 +10,14 @@ class ConnectionManager:
         await websocket.accept()
         data = await websocket.receive_text()
         id_user = json.loads(data)
-
         self.active_user_connections[int(id_user["id_user"])] = websocket
+        print(f"User => {id_user} connected", flush=True)
 
     async def disconnect(self, websocket: WebSocket):
-        data = await websocket.receive_text()
-        id_user = json.loads(data)
-        del self.active_user_connections[int(id_user["id_user"])]
+        pass
+        #data = await websocket.receive_text()
+        #id_user = json.loads(data)
+        #del self.active_user_connections[int(id_user["id_user"])]
 
     async def send_message(self, user_id, friends):
         for friend in friends:
