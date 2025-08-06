43bccacded986534ec351a5a54710dc2a8e2799b
Rudolf Bakhman <rudik.login@gmail.com>
2025-08-06 17:45:02 +0300

    add send message from user to user

--- backend/app/users/messages_router.py
+++ backend/app/users/messages_router.py
@@ -7,15 +7,15 @@ from app.users import dependencies as dep
 router = APIRouter()
 
 
-@router.post("/dialog/{user_id}/send/{from_user}")
-def send_message_to_user(user_id: int, from_user: int, user_message: UserMessage):
+@router.post("/dialog/{from_user}/send/{to_user}")
+def send_message_to_user(from_user: int, to_user: int, user_message: UserMessage):
     # token: str = Depends(dep.get_token)
     # user_id_from_token = dep.get_current_user(token)
 
     try:
-        result = mongo_db.mongodb_insert_one(from_user, user_id, user_message.message)
+        result = mongo_db.mongodb_insert_one(from_user, to_user, user_message.message)
         if result:
-            return {"Message send": "ok", "from": from_user, "to": user_id}
+            return {"Message send": "ok", "from": from_user, "to": to_user}
         else:
             return {"Message send": "false"}
     except BaseException:
