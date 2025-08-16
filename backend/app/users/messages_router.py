538ac33cc7e7d17a434793c49c06faa34fddc543
Rudolf Bakhman <rudik.login@gmail.com>
2025-08-16 17:20:21 +0300

    add dialog to redis db

--- backend/app/users/messages_router.py
+++ backend/app/users/messages_router.py
@@ -1,8 +1,10 @@
 from fastapi import APIRouter, HTTPException, Depends, status
 from app.users.schema import UserMessage
-from app.mongo_db import mongo_db
-from app.users import dependencies as dep
+from app.redis_dialogs import redis_db
 
+# temporary not used
+# from app.mongo_db import mongo_db
+# from app.users import dependencies as dep
 
 router = APIRouter()
 
@@ -13,7 +15,11 @@ def send_message_to_user(from_user: int, to_user: int, user_message: UserMessage
     # user_id_from_token = dep.get_current_user(token)
 
     try:
-        result = mongo_db.mongodb_insert_one(from_user, to_user, user_message.message)
+        # dialog in mongo DB
+        #result = mongo_db.mongodb_insert_one(from_user, to_user, user_message.message)
+
+        # dialog in redis DB
+        result = redis_db.redis_db_send_message_from_to(from_user, to_user, user_message.message)
         if result:
             return {"Message send": "ok", "from": from_user, "to": to_user}
         else:
@@ -21,7 +27,25 @@ def send_message_to_user(from_user: int, to_user: int, user_message: UserMessage
     except BaseException:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
-            detail="User Posts not found"
+            detail="User message not send"
+        )
+
+
+@router.post("/dialog_get/{from_user}/send/{to_user}")
+def get_messages_from_to_user(from_user: int, to_user: int):
+    # token: str = Depends(dep.get_token)
+    # user_id_from_token = dep.get_current_user(token)
+
+    try:
+        result = redis_db.redis_db_get_user_messages(from_user, to_user)
+        if result:
+            return result
+        else:
+            return {"Message get": "false"}
+    except BaseException:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="User dialog not found"
         )
 
 
@@ -29,11 +53,11 @@ def send_message_to_user(from_user: int, to_user: int, user_message: UserMessage
 def get_all_messages_from_user(user_id: int):
     try:
         result_query = []
-        for r in mongo_db.mongodb_query(user_id):
+        for r in redis_db.redis_search_user_dialog(user_id):
             result_query.append(r)
         return result_query
     except BaseException:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
-            detail="User Post can Create"
+            detail="Dialog not find"
         )
