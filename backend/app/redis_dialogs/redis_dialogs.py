487c661a9f4b8acf7463675627091975796040b6
Rudolf Bakhman <rudik.login@gmail.com>
2025-08-17 19:38:17 +0300

    add lua to redis

--- backend/app/redis_dialogs/redis_dialogs.py
+++ backend/app/redis_dialogs/redis_dialogs.py
@@ -5,6 +5,34 @@ from datetime import datetime
 from app.config import settings
 
 
+# Lua scrips
+send_message = ("local data = {"
+                "from_user = ARGV[1],"
+                "to_user = ARGV[2],"
+                "date = ARGV[3],"
+                "message = ARGV[4]"
+                "}"
+                "redis.call('RPUSH', KEYS[1], cjson.encode(data))"
+                "return 'ok'"
+                )
+
+get_dialog = ("local dialog = KEYS[1]"
+              "local result = redis.call('LRANGE', dialog, 0, -1)"
+              "return result")
+
+search_dialogs = ("local dialog_one = KEYS[1]"
+                  "local dialog_two = KEYS[2]"
+                  "local db = ARGV[1]"
+                  "local status, _ = redis.call('SELECT', db)"
+                  "if status.ok == 'OK' then "
+                  "local value_one = redis.call('KEYS', dialog_one)"
+                  "local value_two = redis.call('KEYS', dialog_two)"
+                  "for _, value in ipairs(value_two) do "
+                  "table.insert(value_one, value)"
+                  "end return value_one "
+                  "else return 'Error selecting database' end")
+
+
 def redis_connect():
     r_connect = redis.Redis(host=settings.REDIS_DIALOG_HOST,
                             port=settings.REDIS_DIALOG_PORT,
@@ -23,46 +51,32 @@ def find_sort_id(from_user: int, to_user: int) -> tuple:
     return from_user if from_user < to_user else to_user, to_user if to_user > from_user else from_user
 
 
-def redis_db_send_message_from_to(from_user: int, to_user: int, message: str) -> bool:
+def redis_db_send_message_from_to(from_user: int, to_user: int, message: str) -> None:
     r = redis_connect()
     first, last = find_sort_id(from_user, to_user)
 
-    data = {
-        "from_user": from_user,
-        "to_user": to_user,
-        "date": datetime.now().strftime("%Y-%M-%d %H:%m:%S"),
-        "message": message
-    }
+    operation = r.register_script(send_message)
 
-    try:
-        r.rpush(f"dialog:{first}:{last}", json.dumps(data, ensure_ascii=False))
-
-    except redis.exceptions.RedisError as error:
-        print(f"Error send message {error}")
-        return False
-
-    return True
+    return operation(keys=[f"dialog:{first}:{last}"], args=[from_user,
+                                                            to_user,
+                                                            datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+                                                            message]).decode("utf-8")
 
 
-def redis_search_user_dialog(id_user: int) -> list | None:
+def redis_search_user_dialog(id_user: int) -> list:
     r = redis_connect()
-    list_names = []
 
-    for key in r.scan_iter(f"dialog:{id_user}:*"):
-        list_names.append(key)
+    operation = r.register_script(search_dialogs)
+    res = operation(keys=[f"dialog:*:{id_user}", f"dialog:{id_user}:*"], args=[str(1)])
 
-    if list_names:
-        return list_names
-    else:
-        for key in r.scan_iter(f"dialog:*:{id_user}"):
-            list_names.append(key)
-
-    return list_names if list_names else None
+    return [i.decode("utf-8") for i in res]
 
 
 def redis_db_get_user_messages(from_user: int, to_user: int) -> list:
     r = redis_connect()
     first, last = find_sort_id(from_user, to_user)
-    dialog = r.lrange(f"dialog:{first}:{last}", 0, -1)
 
-    return [json.loads(i.decode('utf-8')) for i in dialog]
+    operation = r.register_script(get_dialog)
+    dialog = operation(keys=[f"dialog:{first}:{last}"])
+
+    return [json.loads(i.decode("utf-8")) for i in dialog]
